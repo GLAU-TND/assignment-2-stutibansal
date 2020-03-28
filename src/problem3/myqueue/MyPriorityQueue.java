66d3c1c13ded9bf7c0163ece0efc5b3e98a48931
stutibansal <48116152+stutibansal@users.noreply.github.com>
2020-03-28 15:08:55 +0530

    Update MyPriorityQueue.java
    Updated

--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -5,20 +5,41 @@
  *  Time: 7:15 PM
  */
 package problem3.myqueue;
-import problem3.myLinkedList.MyLinkedList;
-import problem3.student.Student;
+import problem3.node.Node;
 
 public class MyPriorityQueue {
-MyLinkedList linkedList = new MyLinkedList();
-    public void enqueue(Student student){
-        linkedList.addLast(student);
+private Node front;
+
+    public Node getFront() {
+        return front;
     }
 
-    public void dequeue(){
-        linkedList.deleteFirst();
+    public void setFront(Node front) {
+        this.front = front;
     }
-    public void show(){
-        linkedList.display();
+
+    public void enQueue(Node newNode) {
+        Node temp = getFront();
+        if (getFront() == null || getFront().getStudent().getRollNumber() > newNode.getStudent().getRollNumber()) {
+            setFront(newNode);
+            getFront().setNextNode(temp);
+        } else {
+            while (temp.getNextNode() != null && temp.getNextNode().getStudent().getRollNumber() <= newNode.getStudent().getRollNumber()) {
+                temp = temp.getNextNode();
+            }
+            newNode.setNextNode(temp.getNextNode());
+            temp.setNextNode(newNode);
+        }
+    }
+
+    public Node deQueue() {
+        if (getFront() == null) {
+            System.out.println("Empty queue");
+            return null;
+        }
+        Node temp = getFront();
+        setFront(getFront().getNextNode());
+        return temp;
     }
 
 }
