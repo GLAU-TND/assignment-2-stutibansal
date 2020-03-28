a77ac75f06247b427df852bcc505ffed403bd1ff
stutibansal <48116152+stutibansal@users.noreply.github.com>
2020-03-28 15:17:27 +0530

    Update MyBinarySearchTree.java
    Updated

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -4,38 +4,21 @@
  *  Date: 23-Mar-20
  *  Time: 7:17 PM
  */
+
 package problem1.mybst;
+
 import problem1.node.TreeNode;
+
+import java.util.ArrayList;
 
 // to implement BinarySearchTree
 public class MyBinarySearchTree {
-private TreeNode root;
-    private int countRight;
 
-    public void insert(int data){
-        TreeNode node = new TreeNode(data);
-        if(root==null){
-            root=node;
-        }
-        else{
-            TreeNode current=root;
-            TreeNode parent = null;
-            while(current!=null){
-                parent=current;
-                if(node.getData()<current.getData()){
-                    current=current.getLeft();
-                }
-                else{
-                    current=current.getRight();
-                }
-            }
-            if(node.getData()<parent.getData()){
-                parent.setLeft(node);
-            }
-            else{
-                parent.setRight(node);
-            }
-        }
+    private TreeNode root;
+    private ArrayList<Integer> arrayList;
+
+    public MyBinarySearchTree() {
+        arrayList = new ArrayList<>();
     }
 
     public TreeNode getRoot() {
@@ -46,55 +29,76 @@ private TreeNode root;
         this.root = root;
     }
 
-    public void preOrder(TreeNode temp){
-        if(temp==null){
+    public void insert(TreeNode newNode) {
+
+        if (getRoot() == null) {
+            setRoot(newNode);
             return;
         }
-        else{
-            System.out.println(temp.getData());
-            preOrder(temp.getLeft());
-            preOrder(temp.getRight());
-        }
-    }
 
-    public void inOrder(TreeNode temp){
-        if(temp==null){
-            return;
-        }
-        else{
-
-            inOrder(temp.getLeft());
-            System.out.println(temp.getData());
-            inOrder(temp.getRight());
-        }
-    }
-
-    public int getCountRight() {
-        return countRight;
-    }
-
-    public void setCountRight(int countRight) {
-        this.countRight = countRight;
-    }
-
-    public void traverse(){
-        TreeNode current = root;
         TreeNode parent = null;
-        this.countRight=0;
-        while(current!=null){
-            if(current.getLeft()==null) {
-                countRight++;
-            }
-            if(parent.getData()==current.getData()){
+        TreeNode current = getRoot();
+        boolean isLeft = true;
 
-            }
-            if(parent.getData()<current.getData()) {
-                System.out.println(current.getData());
-                current = current.getLeft();
-            }
-            if(parent.getData()>current.getData()){
-                current = current.getRight();
+        while (current != null) {
+            parent = current;
+            if (newNode.getData() > current.getData()) {
+                current = current.getRightNode();
+                isLeft = false;
+            } else {
+                current = current.getLeftNode();
+                isLeft = true;
             }
         }
+            if (isLeft)
+                parent.setLeftNode(newNode);
+            else
+                parent.setRightNode(newNode);
+    }
+
+    public int showLeft(TreeNode node, boolean isLeft) {
+        int c = 0;
+
+        if (node == null)
+            return c;
+
+        if (isLeft)
+            System.out.println(node);
+
+        if (node.getLeftNode() == null)
+            c++;
+
+        c += showLeft(node.getLeftNode(), true);
+        c += showLeft(node.getRightNode(), false);
+        return c;
+    }
+
+    public void showTreeData(TreeNode node) {
+        if (node == null)
+            return;
+        System.out.println(node);
+        showTreeData(node.getLeftNode());
+        showTreeData(node.getRightNode());
+    }
+
+    private void addElementToList(TreeNode node) {
+        if (node == null)
+            return;
+        arrayList.add(node.getData());
+        addElementToList(node.getLeftNode());
+        addElementToList(node.getRightNode());
+    }
+
+    public ArrayList<Integer> getPreOrderList() {
+        addElementToList(getRoot());
+        return arrayList;
+    }
+
+    public void postOrderTraversal(TreeNode node) {
+        if (node == null)
+            return;
+        postOrderTraversal(node.getLeftNode());
+        postOrderTraversal(node.getRightNode());
+        System.out.println(node);
     }
 }
