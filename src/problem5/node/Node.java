d6f74a340ccf1ef05f4f146ba406b4d7ffdf3549
stutibansal <48116152+stutibansal@users.noreply.github.com>
2020-03-28 14:57:17 +0530

    Update Node.java
    Created

--- src/problem5/node/Node.java
+++ src/problem5/node/Node.java
@@ -7,4 +7,23 @@
 package problem5.node;
 // to define node properties
 public class Node {
+private Student student;
+    private Node next;
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
 }
